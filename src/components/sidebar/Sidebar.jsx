55d0598e5c513c836e5aafd34d58ecf68440ea8f
michelle-vu19 <michelle.vu19@gmail.com>
2022-12-08 23:10:57 -0500

    commenting out close sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,54 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import './sidebar.scss'
-import { Link, useLocation } from 'react-router-dom'
-import sidebarNav from '../../configs/sidebarNav'
+import React, { useEffect, useState } from "react";
+import "./sidebar.scss";
+import { Link, useLocation } from "react-router-dom";
+import sidebarNav from "../../configs/sidebarNav";
 
 const Sidebar = () => {
-    const [activeIndex, setActiveIndex] = useState(0)
-    const location = useLocation()
+  const [activeIndex, setActiveIndex] = useState(0);
+  const location = useLocation();
 
-    useEffect(() => {
-        const curPath = window.location.pathname.split('/')[1]
-        const activeItem = sidebarNav.findIndex(item => item.section === curPath)
+  useEffect(() => {
+    const curPath = window.location.pathname.split("/")[1];
+    const activeItem = sidebarNav.findIndex((item) => item.section === curPath);
 
-        setActiveIndex(curPath.length === 0 ? 0 : activeItem)
-    }, [location])
+    setActiveIndex(curPath.length === 0 ? 0 : activeItem);
+  }, [location]);
 
-    const closeSidebar = () => {
-        document.querySelector('.main__content').style.transform = 'scale(1) translateX(0)'
-        setTimeout(() => {
-            document.body.classList.remove('sidebar-open')
-            document.querySelector('.main__content').style = ''
-        }, 500);
-    }
+  const closeSidebar = () => {
+    // document.querySelector('.main__content').style.transform = 'scale(1) translateX(0)'
+    // setTimeout(() => {
+    //     document.body.classList.remove('sidebar-open')
+    //     document.querySelector('.main__content').style = ''
+    // }, 500);
+  };
 
-    return (
-        <div className='sidebar'>
-            <div className="sidebar__logo">
-                <div className="sidebar-close" onClick={closeSidebar}>
-                    <i className='bx bx-x'></i>
-                </div>
-            </div>
-            <div className="sidebar__menu">
-                {
-                    sidebarNav.map((nav, index) => (
-                        <Link to={nav.link} key={`nav-${index}`} className={`sidebar__menu__item ${activeIndex === index && 'active'}`} onClick={closeSidebar}>
-                            <div className="sidebar__menu__item__icon">
-                                {nav.icon}
-                            </div>
-                            <div className="sidebar__menu__item__txt">
-                                {nav.text}
-                            </div>
-                        </Link>
-                    ))
-                }
-                <div className="sidebar__menu__item">
-                    
-                    
-                </div>
-            </div>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__logo">
+        <div className="sidebar-close" onClick={closeSidebar}>
+          <i className="bx bx-x"></i>
         </div>
-    )
-}
+      </div>
+      <div className="sidebar__menu">
+        {sidebarNav.map((nav, index) => (
+          <Link
+            to={nav.link}
+            key={`nav-${index}`}
+            className={`sidebar__menu__item ${
+              activeIndex === index && "active"
+            }`}
+            onClick={closeSidebar}
+          >
+            <div className="sidebar__menu__item__icon">{nav.icon}</div>
+            <div className="sidebar__menu__item__txt">{nav.text}</div>
+          </Link>
+        ))}
+        <div className="sidebar__menu__item"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
